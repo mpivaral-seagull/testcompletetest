535979a4b9354767f79ab423ace9621104c21a98
Marvin Pivaral <mpivaral@seagullscientific.com>
2025-03-05 15:42:22 -0600

    First Commit

---
+++ GitIntegration/Script/TestWithBrowser.js
@@ -0,0 +1,11 @@
+﻿
+function test(){
+  let page = "https://www.smartbear.com/";
+  Browsers.Item(btChrome).Run(page);
+  Sys.Browser().Page("https://smartbear.com/").FindElement("//li[contains(., 'Products')]").Click();
+  Sys.Browser().Page("https://smartbear.com/").FindElement("//a[contains(., 'TestComplete')]").Click();
+  
+  if (Sys.Browser().Page("https://smartbear.com/product/testcomplete/").WaitElement("//a[.='Start My Free Trial']", 5000).Exists) {
+    Sys.Browser().Page("https://smartbear.com/product/testcomplete/").FindElement("//a[.='Start My Free Trial']").Click();
+  }
+}
